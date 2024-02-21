b0a3d13c30880bc0c3faa09e995d625d4021d04f
JuanDied <juanrayadi2014@gmail.com>
2024-02-21 15:42:08 -0500

    [ADD]Se crean las capas para Events repository, persitance, service. faltaría Controller

---
+++ Backend/src/main/java/com/example/demo/persistance/EventDAOImp.java
@@ -0,0 +1,38 @@
+package com.example.demo.persistance;
+
+import com.example.demo.entities.Event;
+import com.example.demo.repository.EventRepository;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class EventDAOImp implements IEventDAO{
+
+    private final EventRepository eventRepository;
+
+    public EventDAOImp(EventRepository eventRepository) {
+        this.eventRepository = eventRepository;
+    }
+
+    @Override
+    public List<Event> findAll() {
+        return (List<Event>) eventRepository.findAll();
+    }
+
+    @Override
+    public Optional<Event> findById(Long eventId) {
+        return eventRepository.findById(eventId);
+    }
+
+    @Override
+    public void save(Event event) {
+        eventRepository.save(event);
+    }
+
+    @Override
+    public void deleteById(Long eventId) {
+        eventRepository.deleteById(eventId);
+    }
+}
---
+++ Backend/src/main/java/com/example/demo/service/impl/EventServiceImpl.java
@@ -0,0 +1,41 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.entities.Event;
+import com.example.demo.persistance.IEventDAO;
+import com.example.demo.service.IEventService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class EventServiceImpl implements IEventService {
+
+
+    private final IEventDAO eventDAO;
+
+    public EventServiceImpl(IEventDAO eventDAO) {
+        this.eventDAO = eventDAO;
+    }
+
+
+    @Override
+    public List<Event> findAll() {
+        return eventDAO.findAll();
+    }
+
+    @Override
+    public Optional<Event> findById(Long eventId) {
+        return eventDAO.findById(eventId);
+    }
+
+    @Override
+    public void save(Event event) {
+        eventDAO.save(event);
+    }
+
+    @Override
+    public void deleteById(Long eventId) {
+        eventDAO.deleteById(eventId);
+    }
+}
