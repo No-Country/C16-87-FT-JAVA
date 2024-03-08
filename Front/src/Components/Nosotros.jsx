46254f38cc53ff3f8a0b85fbeddee0ea148e963b
Nicolas Fontanini <anicolasfontanini@gmail.com>
2024-03-07 22:50:27 -0300

    actualize las fotos y archivo datos . json

--- Front/src/Components/Nosotros.jsx
+++ Front/src/Components/Nosotros.jsx
@@ -5,31 +5,34 @@ import Navbar from "../components/Navbar";
 import { FaGithub, FaLinkedinIn } from "react-icons/fa";
 
 const Perfil = () => {
-  const { nombre, apellido, formacion, rol, github, linkedin, image } = datosJson;
+  const profiles = datosJson.map((perfil, index) => (
+    <div key={index} className="perfil-container bg-white p-6 rounded-lg shadow-md mb-4">
+      <img
+        src={perfil.image}
+        alt={`${perfil.nombre} ${perfil.apellido}`}
+        className="w-32 h-32 object-cover rounded-full mx-auto mb-4"
+      />
+      <h1 className="text-2xl font-bold text-gray-800 mb-2">{`${perfil.nombre} ${perfil.apellido}`}</h1>
+      <p className="text-gray-600 mb-2">Formación: {perfil.formacion}</p>
+      <p className="text-gray-600 mb-4">Rol: {perfil.rol}</p>
+      <div className="iconos flex justify-center">
+        <a href={perfil.github} target="_blank" rel="noopener noreferrer">
+          <FaGithub className="text-4xl text-gray-800 mx-2 hover:text-gray-600" />
+        </a>
+        <a href={perfil.linkedin} target="_blank" rel="noopener noreferrer">
+          <FaLinkedinIn className="text-4xl text-gray-800 mx-2 hover:text-gray-600" />
+        </a>
+      </div>
+    </div>
+  ));
 
   return (
     <>
       <Navbar></Navbar>
-      <div className="perfil-container bg-white p-6 rounded-lg shadow-md">
-        <img
-          src={image}
-          alt={`${nombre} ${apellido}`}
-          className="w-32 h-32 object-cover rounded-full mx-auto mb-4"
-        />
-        <h1 className="text-2xl font-bold text-gray-800 mb-2">{`${nombre} ${apellido}`}</h1>
-        <p className="text-gray-600 mb-2">Formación: {formacion}</p>
-        <p className="text-gray-600 mb-4">Rol: {rol}</p>
-        <div className="iconos flex justify-center">
-          <a href={github} target="_blank" rel="noopener noreferrer">
-            <FaGithub className="text-4xl text-gray-800 mx-2 hover:text-gray-600" />
-          </a>
-          <a href={linkedin} target="_blank" rel="noopener noreferrer">
-            <FaLinkedinIn className="text-4xl text-gray-800 mx-2 hover:text-gray-600" />
-          </a>
-        </div>
-      </div>
+      {profiles}
     </>
   );
 };
 
 export default Perfil;
+
